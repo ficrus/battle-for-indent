c86b8def2a12d14c00643a791503e78a096e9ca3
trrttrtrsrfewrwed <timurruskiy2@mail.ru>
2019-03-26 23:27:52 +0300

    Added singleton pattern to factories

--- unit_factories.py
+++ unit_factories.py
@@ -1,30 +1,28 @@
 from units import *
 
 
-class UnitFactory(ABC):
-    def __init__(self):
-        pass
+class Singleton(ABC, type):
+    _instances = {}
 
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class UnitFactory(metaclass=Singleton):
     @abstractmethod
     def create(self):
         pass
 
 
 class KnightFactory(UnitFactory):
-    def __init__(self):
-        print('Knight Factory Created')
-        super().__init__()
-
     def create(self) -> BaseUnit:
         print('KnightFactory creates knight')
         return Knight()
 
 
 class BanditFactory(UnitFactory):
-    def __init__(self):
-        print('Bandit Factory Created')
-        super().__init__()
-
     def create(self) -> BaseUnit:
         print('BanditFactory creates bandit')
         return Bandit()
