0f235e791579cbe927c55e2cbfe3b7b5fac9a8fe
artyomnurgaliev <artyomnurgaliev@mail.ru>
2019-03-28 23:35:13 +0300

    change example to demonstrate two armies simultaneously

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
-
+from game import *
+import unit_factories
 from textbutton import *
 import sprites
 
@@ -33,36 +34,63 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.color.LIGHT_GREEN)
 
         self.sprite_factory = sprites.SpriteFactory()
-        self.bandit_list = None
-        self.knight_list = None
+        self.bandit_sprite_list_1 = None
+        self.bandit_sprite_list_2 = None
+        self.knight_sprite_list_1 = None
+        self.knight_sprite_list_2 = None
         self.button_list = None
 
+        self.knight_factory = None
+        self.bandit_factory = None
+        self.army1 = None
+        self.army2 = None
+        self.game = None
+
     def setup(self):
+        self.game = Game()
+        self.army1 = Army()
+        self.army2 = Army()
+        self.knight_factory = unit_factories.KnightFactory()
+        self.bandit_factory = unit_factories.BanditFactory()
+        self.sprite_factory = sprites.SpriteFactory()
+
         # Create your sprites and sprite lists here
-        self.bandit_list = arcade.SpriteList()
-        self.knight_list = arcade.SpriteList()
+        self.bandit_sprite_list_1 = arcade.SpriteList()
+        self.bandit_sprite_list_2 = arcade.SpriteList()
+        self.knight_sprite_list_1 = arcade.SpriteList()
+        self.knight_sprite_list_2 = arcade.SpriteList()
 
         # Create our on-screen GUI buttons
         self.button_list = []
 
-        add_knight_button = AddKnightButton(60, 570, self.add_knight)
-        self.button_list.append(add_knight_button)
+        add_knight_button_1 = AddKnightButton(60, 570, self.add_knight_1)
+        self.button_list.append(add_knight_button_1)
 
-        add_bandit_button = AddBanditButton(60, 515, self.add_bandit)
-        self.button_list.append(add_bandit_button)
+        add_bandit_button_1 = AddBanditButton(60, 515, self.add_bandit_1)
+        self.button_list.append(add_bandit_button_1)
+
+        add_knight_button_2 = AddKnightButton(SCREEN_WIDTH/2+60, 570, self.add_knight_2)
+        self.button_list.append(add_knight_button_2)
+
+        add_bandit_button_2 = AddBanditButton(SCREEN_WIDTH/2+60, 515, self.add_bandit_2)
+        self.button_list.append(add_bandit_button_2)
 
     def on_draw(self):
         arcade.start_render()
 
-        self.bandit_list.draw()
-        self.knight_list.draw()
+        self.bandit_sprite_list_1.draw()
+        self.bandit_sprite_list_2.draw()
+        self.knight_sprite_list_1.draw()
+        self.knight_sprite_list_2.draw()
 
         for button in self.button_list:
             button.draw()
 
     def update(self, delta_time):
-        self.bandit_list.update()
-        self.knight_list.update()
+        self.bandit_sprite_list_1.update()
+        self.bandit_sprite_list_2.update()
+        self.knight_sprite_list_1.update()
+        self.knight_sprite_list_2.update()
 
     def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
         Check.check_mouse_enter_for_buttons(x, y, self.button_list)
@@ -73,17 +101,41 @@ class MyGame(arcade.Window):
     def on_mouse_release(self, x, y, button, key_modifiers):
         Check.check_mouse_release_for_buttons(x, y, self.button_list)
 
-    def add_knight(self):
-        knight = self.sprite_factory.create_unit_sprite(unit='Knight')
-        knight.center_x = len(self.knight_list)*100
-        knight.center_y = SCREEN_HEIGHT * 2/3
-        self.knight_list.append(knight)
+    def add_knight_1(self):
+        if len(self.knight_sprite_list_1) < 12:
+            knight = self.knight_factory.create()
+            self.army1.add_unit(knight)
+            knight_sprite = self.sprite_factory.create_unit_sprite(unit='Knight')
+            knight_sprite.center_x = 50 + len(self.knight_sprite_list_1) * 50
+            knight_sprite.center_y = SCREEN_HEIGHT * 2/3
+            self.knight_sprite_list_1.append(knight_sprite)
 
-    def add_bandit(self):
-        bandit = self.sprite_factory.create_unit_sprite(unit='Bandit')
-        bandit.center_x = len(self.bandit_list) * 100
-        bandit.center_y = SCREEN_HEIGHT / 3
-        self.bandit_list.append(bandit)
+    def add_bandit_1(self):
+        if len(self.bandit_sprite_list_1) < 12:
+            bandit = self.bandit_factory.create()
+            self.army1.add_unit(bandit)
+            bandit_sprite = self.sprite_factory.create_unit_sprite(unit='Bandit')
+            bandit_sprite.center_x = 50 + len(self.bandit_sprite_list_1) * 50
+            bandit_sprite.center_y = SCREEN_HEIGHT / 3
+            self.bandit_sprite_list_1.append(bandit_sprite)
+
+    def add_knight_2(self):
+        if len(self.knight_sprite_list_2) < 12:
+            knight = self.knight_factory.create()
+            self.army2.add_unit(knight)
+            knight_sprite = self.sprite_factory.create_unit_sprite(unit='Knight')
+            knight_sprite.center_x = SCREEN_WIDTH / 2 + 50 + len(self.knight_sprite_list_2) * 50
+            knight_sprite.center_y = SCREEN_HEIGHT * 2 / 3
+            self.knight_sprite_list_2.append(knight_sprite)
+
+    def add_bandit_2(self):
+        if len(self.bandit_sprite_list_2) < 12:
+            bandit = self.bandit_factory.create()
+            self.army2.add_unit(bandit)
+            bandit_sprite = self.sprite_factory.create_unit_sprite(unit='Bandit')
+            bandit_sprite.center_x = SCREEN_WIDTH / 2 + 50 + len(self.bandit_sprite_list_2) * 50
+            bandit_sprite.center_y = SCREEN_HEIGHT / 3
+            self.bandit_sprite_list_2.append(bandit_sprite)
 
 
 def main():
