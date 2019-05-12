77352f131417606434efdf26b7b1c117e45287d0
Sergey Gorbunov <eaglemango@gmail.com>
2019-05-13 00:04:38 +0300

    Some fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="battle-for-indent-ficrus",
-    version="0.0.1",
+    version="0.0.2",
     author="ficrus",
     author_email="eaglemango@gmail.com",
     description="Open-source strategy game",
